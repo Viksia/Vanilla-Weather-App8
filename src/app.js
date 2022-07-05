e9b7106837a58fca3eabb57e2cc8f7bb02c66441
Viksia <105889022+Viksia@users.noreply.github.com>
2022-07-05 22:27:20 +0200

    Добавила ид нижней панели прогноза

--- src/app.js
+++ src/app.js
@@ -23,6 +23,29 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+//Добавляем колонки прогнозов внизу апп
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = `
+  <div class="row">
+   <div class="col-2">
+    <div class="weather-forecast-date">
+                                Fri
+      </div>
+      <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="" width="42">
+     <div class="weather-forecast-temperature">
+      <span class="weather-forecast-temperature-max">
+                                    18°
+      </span>
+      <span class="weather-forecast-temperature-min">
+                                    12°
+      </span>
+      </div>
+    </div>
+</div>`;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -90,3 +113,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
+displayForecast();
